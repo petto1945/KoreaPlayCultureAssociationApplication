65a392714a64cd566fe652633f56ef90b71f5e52
gimgeonhun <gimgeonhun@172.30.1.8>
2019-03-04 17:17:04 +0900

    2018.03.04 first patch Test

--- src/main/java/korea/play/culture/association/KoreaPlayCultureAssociationApplication.java
+++ src/main/java/korea/play/culture/association/KoreaPlayCultureAssociationApplication.java
@@ -10,5 +10,6 @@ public class KoreaPlayCultureAssociationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KoreaPlayCultureAssociationApplication.class, args);
+		System.out.println("Hi git compaer");
 	}
 }
